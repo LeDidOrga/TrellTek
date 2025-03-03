a8818ba2883cc51b6a46dd7a7144673361ed0d00
HugoBernier <hugo.bernier@gmail.com>
2025-03-03 15:10:36 +0100

    fix: env fetch

--- src/api/trelloClient.js
+++ src/api/trelloClient.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 const TRELLO_API_URL = 'https://api.trello.com/1';
-const TRELLO_API_KEY = process.env.TRELLO_API_KEY;
-const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
+const TRELLO_API_KEY = import.meta.env.VITE_TRELLO_API_KEY;
+const TRELLO_TOKEN = import.meta.env.VITE_TRELLO_TOKEN;
 
 const trelloClient = axios.create({
     baseURL: TRELLO_API_URL,

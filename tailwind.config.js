65f281e1cd650c0bb5f5f2fe47d050ee64468248
hugo <hugo.bernier88@gmail.com>
2025-03-31 11:20:24 +0200

    fix: added font tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontSize: {
+        sm: '0.750rem',
+        base: '1rem',
+        xl: '1.333rem',
+        '2xl': '1.777rem',
+        '3xl': '2.369rem',
+        '4xl': '3.158rem',
+        '5xl': '4.210rem',
+      },
+      fontFamily: {
+        heading: 'Comfortaa',
+        body: 'Quicksand',
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
       colors: {
         text: {
           50: '#1a0e00',
